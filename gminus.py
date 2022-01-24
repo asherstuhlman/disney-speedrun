a46d6a7e231ff9dbf4dcb97919ddcbb97371559b
asherstuhlman <asherstuhlman@gmail.com>
2022-01-23 18:10:14 -0600

    chabnges

--- gminus.py
+++ gminus.py
@@ -63,11 +63,6 @@ def updateWaitRatio(df):
             waitRatio = 1
             df.at[row[0],"wait_ratio"] = waitRatio
     return df
-    """
-    with pysftp.Connection('server90.web-hosting.com', username='stuhazjf', password='QXNoLW1hbjE=') as sftp:
-        with sftp.cd('/public_html/images'):           # temporarily chdir 
-            sftp.put('/filename')  	# upload file to images/filename
-    """
 
 #todo: use https://github.com/cubehouse/themeparks for park open/close times so i don't waste cycles
 
@@ -86,51 +81,54 @@ def addLatLon(df): #also correct ride names for formatting
     return df
 
 def main():
-    now = datetime.now(pytz.timezone("US/Pacific")) #Disneyland timezone
-    if (now.hour < 8) or (now.hour == 23 and now.minute > 55):
-        exit() #Don't run except between 8-midnight
+    while True:
+        now = datetime.now(pytz.timezone("US/Pacific")) #Disneyland timezone
+        if (now.hour < 8) or (now.hour == 23 and now.minute > 55):
+            exit() #Don't run except between 8-midnight
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"}
-    dis_waits_json = requests.get('https://queue-times.com/en-US/parks/16/queue_times.json',headers=headers).json()
-    dca_waits_json = requests.get('https://queue-times.com/en-US/parks/17/queue_times.json',headers=headers).json()
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"}
+        dis_waits_json = requests.get('https://queue-times.com/en-US/parks/16/queue_times.json',headers=headers).json()
+        dca_waits_json = requests.get('https://queue-times.com/en-US/parks/17/queue_times.json',headers=headers).json()
 
-    waits_csv_today = 'disney_waits_' + str(now.month) + "-" + str(now.day) + ".csv"
-    waits_json_today = 'js/ride_data_x.js'
-    date_js_today = 'js/update_date.js'
+        waits_csv_today = 'disney_waits_' + str(now.month) + "-" + str(now.day) + ".csv"
+        waits_json_today = 'js/ride_data_x.js'
+        date_js_today = 'js/update_date.js'
 
-    if not os.path.exists(waits_csv_today): #Check if we already have a file. If we don't already have a file, make one.
-        #This means that the Json gets overwritten even if it's a new day! We don't need yesterday's json.
-        df = pd.DataFrame(columns = ['id','name','current_wait','wait_ratio','lat','lon','park','single_rider','lightning_lane','individual_lightning_lane'])
-        df = appendRides(df,dis_waits_json)
-        df = appendRides(df,dca_waits_json)
-        df = addLatLon(df)
-    else:
-        df = pd.read_csv(waits_csv_today,encoding='latin1')
-    
-    next_check = datetime.now() #MISNAMED RIGHT NOW
-    if (now.minute) < 10:
-        minute_now = "0" + str(now.minute)
-    else:
-        minute_now = str(now.minute) #make sure the minute has two digits so we can sort by minute
+        if not os.path.exists(waits_csv_today): #Check if we already have a file. If we don't already have a file, make one.
+            #This means that the Json gets overwritten even if it's a new day! We don't need yesterday's json.
+            df = pd.DataFrame(columns = ['id','name','current_wait','wait_ratio','lat','lon','park','single_rider','lightning_lane','individual_lightning_lane'])
+            df = appendRides(df,dis_waits_json)
+            df = appendRides(df,dca_waits_json)
+            df = addLatLon(df)
+        else:
+            df = pd.read_csv(waits_csv_today,encoding='latin1')
+        
+        next_check = datetime.now() #MISNAMED RIGHT NOW
+        if (now.minute) < 10:
+            minute_now = "0" + str(now.minute)
+        else:
+            minute_now = str(now.minute) #make sure the minute has two digits so we can sort by minute
 
-    next_col_name = "z" + str(next_check.year) + "-" + str(next_check.month) + "-" + str(next_check.day) + "-" + str(next_check.hour) + "-" + minute_now #create a new column with the current time
-    df[next_col_name] = ''
-    #create a new column with the date/time
+        next_col_name = "z" + str(next_check.year) + "-" + str(next_check.month) + "-" + str(next_check.day) + "-" + str(next_check.hour) + "-" + minute_now #create a new column with the current time
+        df[next_col_name] = ''
+        #create a new column with the date/time
 
-    df = addWaitTimes(df,dis_waits_json,next_col_name)
-    df = addWaitTimes(df,dca_waits_json,next_col_name) #these two amend the latest wait times to the newest column
+        df = addWaitTimes(df,dis_waits_json,next_col_name)
+        df = addWaitTimes(df,dca_waits_json,next_col_name) #these two amend the latest wait times to the newest column
 
-    df = updateWaitRatio(df)
+        df = updateWaitRatio(df)
 
-    #wait until the next five minutes - should start at a given time for consistency
-    with open (waits_csv_today,'w') as waitfile:
-        waitfile.write(df.to_csv(index=False, line_terminator='\n',encoding='latin1'))
-    with open (waits_json_today,'w') as waitfile:
-        js_to_write = df.to_json()
-        js_to_write = js_to_write.replace("'","")
-        js_to_write = "rdata = '[" + js_to_write + "]';"
-        waitfile.write(js_to_write) #needs to add " rdata = '[  " at the top, and end in "    ]';   ", and remove all apostrophes
-    with open(date_js_today,'w') as datefile:
-        datefile.write("updated = '" + str(next_check.hour) + ":" + minute_now + ", " + str(next_check.month) + "/" + str(next_check.day)+"'")
+        #wait until the next five minutes - should start at a given time for consistency
+        with open (waits_csv_today,'w') as waitfile:
+            waitfile.write(df.to_csv(index=False, line_terminator='\n',encoding='latin1'))
+        with open (waits_json_today,'w') as waitfile:
+            js_to_write = df.to_json()
+            js_to_write = js_to_write.replace("'","")
+            js_to_write = "rdata = '[" + js_to_write + "]';"
+            waitfile.write(js_to_write) #needs to add " rdata = '[  " at the top, and end in "    ]';   ", and remove all apostrophes
+        with open(date_js_today,'w') as datefile:
+            datefile.write("updated = '" + str(next_check.hour) + ":" + minute_now + ", " + str(next_check.month) + "/" + str(next_check.day)+"'")
+
+        sleep(300)
 
 main()
