47cf35865f04a37d52f84a4f641577a88088bc50
asherstuhlman <asherstuhlman@gmail.com>
2022-07-29 15:10:19 -0500

    Updates

---
+++ gm_format_csv.py
@@ -0,0 +1,110 @@
+from datetime import datetime, timedelta
+from time import sleep
+import pandas as pd
+import numpy as np
+import pytz
+from boto.s3.connection import S3Connection
+#just for now - importing the same things as the main gminus_v3 file. We probably won't use all of these.
+
+#2. Predicted wait / current wait (Is the predicted wait higher than the actual wait right now? Good)
+#3. Predicted wait in 30 minutes / predicted wait (Is the predicted wait now better than the wait in 30 minutes? Good. We use predicted weight as the denominator because this is a ratio, and past days might not be representative of current absolute values for wait)
+#4. Current wait / How long you _should_ wait for the ride (average wait time over the course of the day? or personal scoring)
+#TO GET THE PREDICTED WAIT TIMES: Look at the last seven days of files and find all waits for this ride within 2 minutes
+
+#Let's start by using one CSV and getting all relevant data from it.
+#First we need to figure out the average wait time over the course of the day.
+#We do this by taking the first nonzero value and the last nonzero value and removing all the rest.
+#It is true that we don't evenly sample a day - but we sample it close enough to evenly that taking the mean is sufficient.
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+"""last_week_file_array = []
+for i in range(7):
+    last_week_file_array.append[datetime.now(pytz.timezone("US/Central")) - timedelta(days = i+1)]
+    past_dfs = pd.read_csv('C:/Users/Asher/Documents/ride_data_'+str(last_week_file_array[i].month)+'-'+str(last_week_file_array[i].day)+'.csv')
+"""#UNCOMMENT THIS WHEN I HAVE MULTIPLE DAYS OF DATA!!!
+def format_csv_for_predictions(prediction_time_delta = 1):
+    yesterday = datetime.now(pytz.timezone("US/Central")) - timedelta(days = prediction_time_delta)
+
+    y_df = pd.read_csv('http://stuhlman.net/gminus/js/ride_data_'+str(yesterday.month)+'-'+str(yesterday.day)+'.csv', encoding = "ISO-8859-1")
+    out_df = pd.read_csv('http://stuhlman.net/gminus/js/rides_and_ids.csv', encoding = "ISO-8859-1").sort_values(by=['id'])
+
+    y_df = pd.merge(y_df,out_df,how="outer",on=["id","id"]).sort_values(by=['id'])
+    y_df["name_x"] = y_df["name_y"]
+    y_df = y_df.drop(columns="name_y")
+
+    average_waits = []
+
+    for row,rowdata in y_df.iterrows(): 
+        #add each nonzero value 
+        time_sum = 0
+        time_count = 0
+        for i in range(len(rowdata)):
+            if i > 12:
+                if rowdata[i] > 0:
+                    time_sum += rowdata[i]
+                    time_count += 1
+        try:
+            average_waits.append(time_sum/time_count)
+        except ZeroDivisionError:
+            average_waits.append(0)
+
+    out_df['average_wait'] = average_waits
+
+    #"normalize" the array to have minute by minute wait time data
+    #get a list of column names
+    #make a list derived from that that's the minutes from the column names
+    #look up minutes and cross-reference that with the list of column names to find the column(s) with data closest to the minute
+    #colnames = y_df.head()
+    for i in range(8, 24):
+        for j in range (0,60):
+            out_df[str(i).zfill(2)+":"+str(j).zfill(2)] = np.nan
+    pass
+
+    a = 0
+    for row,rowdata in y_df.iterrows(): #first we add the data that we have for minutes - which is incomplete. THIS IS WHERE THE ERROR IS BEING CREATED!!! We should just match on ID and labelDate
+        for i in range(len(y_df.keys())):
+            t = y_df.keys()[i]
+            hour = t[find_nth(t,"-",3)+1:find_nth(t,"-",4)]
+            minute = t[find_nth(t,"-",4)+1:].zfill(2)
+            if t[0] == "z" and int(hour) >= 8:
+                labelDate = hour.zfill(2)+":"+minute
+                try: 
+                    out_df.loc[out_df['id']  == rowdata['id'],labelDate] = int(y_df.loc[y_df['id'] == rowdata['id'],t])
+                except ValueError:
+                    out_df.loc[out_df['id']  == rowdata['id'],labelDate] = 0
+                pass
+        pass
+
+    for row,rowdata in out_df.iterrows(): #Then we extrapolate 
+        for i in range(3,len(rowdata)): 
+            labelDate = out_df.keys()[i]
+            if pd.isnull(rowdata[i]):
+                check_forward = i
+                next_value = -1
+                limit = 0
+                try:
+                    while next_value == -1 and limit < 10:
+                        check_forward += 1
+                        if np.isfinite(rowdata[check_forward]):
+                            next_value = rowdata[check_forward]
+                        limit += 1
+                except IndexError:
+                    check_forward = i
+                    limit = 0
+                    while next_value == -1 and limit < 10:
+                        check_forward -= 1 #check backwards instead if we reach the end!
+                        if np.isfinite(rowdata[check_forward]):
+                            next_value = rowdata[check_forward]
+                        limit += 1
+                out_df.loc[out_df['id']  == rowdata['id'],labelDate] = next_value
+    return (out_df.to_csv())
+    #return (out_df.to_csv('http://stuhlman.net/gminus/js/ride_data_formatted_'+str(yesterday.month)+'-'+str(yesterday.day)+'.csv', encoding = "ISO-8859-1", index = False))
+    #out_df.to_csv('C:/Users/Asher/Documents/ride_data_cleaned_'+str(yesterday.month)+'-'+str(yesterday.day)+'.csv', encoding = "ISO-8859-1", index = False)
+    #Great! Now we have a full wait time chart.
+
---
+++ gminus_v3.py
@@ -0,0 +1,264 @@
+from datetime import datetime, timedelta
+from time import sleep
+import requests
+import pandas as pd
+import os
+import pytz
+import io
+import ftplib
+from boto.s3.connection import S3Connection
+
+import gm_format_csv #from this directory
+
+#this is the version for the web
+#it runs on a cronjob every 5 minutes
+#but only triggers during park open hours
+#We don't have a list of park open hours so we just run it 8 AM - midnight
+
+def appendRides(df,json_file):
+    for i in range(len(json_file["lands"])): 
+        for j in range(len(json_file["lands"][i]["rides"])):
+            id = json_file["lands"][i]["rides"][j]["id"]
+            name = json_file["lands"][i]["rides"][j]["name"]
+            df = df.append({'id':id,'name':name},ignore_index=True)
+    return df
+
+def addWaitTimes(df,json_file,next_col_name): #go through an update file and fill in the wait times for each ride 
+    for i in range(len(json_file["lands"])):
+        for j in range(len(json_file["lands"][i]["rides"])):
+            id = json_file["lands"][i]["rides"][j]["id"]
+            wait = json_file["lands"][i]["rides"][j]["wait_time"]
+            df.loc[df.id==id,next_col_name] = wait   #set the latest wait time for a ride to the current wait time
+            df.loc[df.id==id,"current_wait"] = wait #also update the current wait
+            #This is the wrong way to do this! The correct way should be to generate a column of wait times and then append it.
+    return df
+
+def updateWaitRatio(df):
+    #wait ratio has three factors:
+    #1. Highest wait in the last 45 minutes / current wait (Is the current wait time going down? Good)
+    #2. Predicted wait / current wait (Is the predicted wait higher than the actual wait right now? Good)
+    #3. Predicted wait in 30 minutes / predicted wait (Is the predicted wait now better than the wait in 30 minutes? Good. We use predicted weight as the denominator because this is a ratio, and past days might not be representative of current absolute values for wait)
+    last_col = len(df.columns)
+    #print(df.itertuples())
+    if last_col > 20: #if we have enough data points
+        for row in df.itertuples():
+            #FIRST SECTION: Get #1, the highest wait in the last 45 minutes / current wait
+            current_wait_time = df.iat[row[0],last_col-1]
+            #print(current_wait_time)
+            #last_6_column = df.iloc[[row[0]],-6:]
+            recent_max = max(df.iat[row[0],last_col-1],df.iat[row[0],last_col-2],df.iat[row[0],last_col-3],df.iat[row[0],last_col-4],df.iat[row[0],last_col-5],df.iat[row[0],last_col-6],df.iat[row[0],last_col-7])   #should just be a slice of the df though
+            recent_min = min(df.iat[row[0],last_col-1],df.iat[row[0],last_col-2],df.iat[row[0],last_col-3],df.iat[row[0],last_col-4],df.iat[row[0],last_col-5],df.iat[row[0],last_col-6],df.iat[row[0],last_col-7]) 
+            #get the minimum (and maximum) of the last 50 minutes - this is terrible and there's a cleaner way to do it but righ now it just grabs the last entries individualy            
+
+            try:
+                if recent_max == 0:
+                    waitTimeGoingDownRatio = 0
+                elif current_wait_time > recent_min:
+                    if recent_min == 0:
+                        waitTimeGoingDownRatio = 1 #if a ride has had recent downtime, it's a good choice
+                    elif current_wait_time == recent_max:
+                        waitTimeGoingDownRatio = recent_max / recent_min #if the current value is the highest in the last 25 minutes, the waitRatio is highest/lowest
+                    else:
+                        waitTimeGoingDownRatio = 1 #if the current wait time isn't the highest or the lowest, or they're all the same, the wait ratio is 1
+                else:
+                    waitTimeGoingDownRatio = recent_min / recent_max #if the current value is the lowest in the last 25 minutes, the waitRatio is lowest/highest
+            except ZeroDivisionError: #shouldn't happen anymore
+                waitTimeGoingDownRatio = 0.9 #if it's a zero division, then we messed up somehow, but it happened because recent_min was 0
+
+            yesterday = datetime.now(pytz.timezone("US/Central")) - timedelta(days = 1)
+            y_df = pd.read_csv('http://stuhlman.net/gminus/js/ride_data_cleaned_'+str(yesterday.month)+'-'+str(yesterday.day)+'.csv', encoding = "ISO-8859-1")
+            
+            #2. Predicted wait / current wait (Is the predicted wait higher than the actual wait right now? Good)
+            #3. Predicted wait in 30 minutes / predicted wait (Is the predicted wait now better than the wait in 30 minutes? Good. We use predicted weight as the denominator because this is a ratio, and past days might not be representative of current absolute values for wait)
+            #4. Current wait / How long you _should_ wait for the ride (average wait time over the course of the day? or personal scoring)
+            #TO GET THE PREDICTED WAIT TIMES: Look at the last seven days of files and find all waits for this ride within 2 minutes
+            #This currently just uses yesterday
+            current_time = datetime.now(pytz.timezone("US/Pacific"))
+            future_time = current_time + timedelta(minutes = 45)
+            #format them
+            current_time = str(current_time.hour).zfill(2)+':'+str(current_time.minute).zfill(2)
+            future_time = str(future_time.hour).zfill(2)+':'+str(future_time.minute).zfill(2)
+
+            predictedVsCurrentWait = y_df.at[row[0],current_time] / current_wait_time
+            if current_time.hour < 23: #don't use this measure last hour of the day
+                
+                predictedFutureTimePercentUp = y_df.at[row[0],future_time] / y_df.at[row[0],current_time]
+            else:
+                predictedFutureTimePercentUp = 0
+            currentVsAverage = current_wait_time / y_df.at[row[0],"average_wait"]
+            
+            waitRatio = waitTimeGoingDownRatio + predictedVsCurrentWait + predictedFutureTimePercentUp + currentVsAverage 
+            df.at[row[0],"wait_ratio"] = waitRatio
+            print(waitRatio)
+    else:
+        for row in df.itertuples():
+            waitRatio = 1
+            df.at[row[0],"wait_ratio"] = waitRatio
+    return df
+
+#todo: use https://github.com/cubehouse/themeparks for park open/close times so i don't waste cycles
+
+def addLatLon(df): #also correct ride names for formatting
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"}
+    lat_lon_csv = requests.get('http://stuhlman.net/gminus/disney_ride_lat_lon.csv',headers=headers).text
+    lat_lon = pd.read_csv(io.StringIO(lat_lon_csv))
+    for row in lat_lon.itertuples(): #import latitude and longitude
+        df.loc[df.id==row.id,"name"] = row.truename
+        df.loc[df.id==row.id,"lat"] = row.lat
+        df.loc[df.id==row.id,"lon"] = row.lon
+        df.loc[df.id==row.id,"park"] = row.park
+        df.loc[df.id==row.id,"single_rider"] = row.single_rider
+        df.loc[df.id==row.id,"lightning_lane"] = row.lightning_lane
+        df.loc[df.id==row.id,"individual_lightning_lane"] = row.individual_lightning_lane
+    return df
+
+def save_js_remotely(filename,file):
+    myHostname = "ftp.stuhlman.net"
+    myUsername = 'stuhazjf'
+    myPassword = os.environ['js_pw']
+
+    ftp = ftplib.FTP(myHostname)
+    ftp.set_debuglevel(2)
+    ftp.login(myUsername,myPassword)
+
+    ftpResponseMessage = ftp.cwd("/public_html/gminus/js");
+    print(ftpResponseMessage)
+
+    file_to_ftp = io.BytesIO(file.encode('utf-8'))
+    
+    ftpResponseMessage = ftp.storbinary("STOR "+filename,file_to_ftp)
+    print(ftpResponseMessage)
+
+    ftp.quit()
+
+def save_html_remotely(filename,file): #it's stupid that I have two different functions for this when _actually_ these functions save to separate locations and not based on filetype
+    myHostname = "ftp.stuhlman.net"
+    myUsername = 'stuhazjf'
+    myPassword = os.environ['js_pw']
+
+    ftp = ftplib.FTP(myHostname)
+    ftp.set_debuglevel(2)
+    ftp.login(myUsername,myPassword)
+
+    ftpResponseMessage = ftp.cwd("/public_html/gminus");
+    print(ftpResponseMessage)
+
+    file_to_ftp = io.BytesIO(file.encode('utf-8'))
+    
+    ftpResponseMessage = ftp.storbinary("STOR "+filename,file_to_ftp)
+    print(ftpResponseMessage)
+
+    ftp.quit()
+
+def clean_waits(df):
+    js_waitfile = df.to_json()
+    js_waitfile = js_waitfile.replace("'","") #remove apostrophes
+    js_waitfile = js_waitfile.replace(r"\u00c3\u0083\u00c2\u0083","") #idk why but this garbage data keeps getting added so let's cut it out
+    js_waitfile = "rdata = '[" + js_waitfile + "]';"
+    return js_waitfile
+
+def rename_js_with_question_mark(htmlfile,datetxt): #TAKES IN AN HTML FILE and the date in text form to append
+    htmlfile2 = htmlfile.replace('js/update_date.js', 'js/update_date.js?'+datetxt) 
+    htmlfile3 = htmlfile2.replace('js/ride_data.js', 'js/ride_data.js?'+datetxt)
+    return htmlfile3
+
+
+def main():
+    for do_exactly_twice in range(2):
+        print("Hello!")
+        now = datetime.now(pytz.timezone("US/Pacific")) #Disneyland timezone
+        if (now.hour < 5) or (now.hour == 23 and now.minute > 55):
+            exit() #Don't run except while a park is open
+
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0"}
+        dis_waits_json = requests.get('https://queue-times.com/en-US/parks/16/queue_times.json',headers=headers).json()
+        dca_waits_json = requests.get('https://queue-times.com/en-US/parks/17/queue_times.json',headers=headers).json()
+        epcot_waits_json = requests.get('https://queue-times.com/en-US/parks/5/queue_times.json',headers=headers).json()
+        mk_waits_json = requests.get('https://queue-times.com/en-US/parks/6/queue_times.json',headers=headers).json()
+        dhs_waits_json = requests.get('https://queue-times.com/en-US/parks/7/queue_times.json',headers=headers).json()
+        ak_waits_json = requests.get('https://queue-times.com/en-US/parks/8/queue_times.json',headers=headers).json()
+        last_updated = requests.get('http://stuhlman.net/gminus/js/update_date.txt').content[3:5] #get the day
+
+        if int(last_updated) != now.day:
+            #create a new file with formatted minute-by-minute data
+            yesterday = datetime.now(pytz.timezone("US/Pacific")) - timedelta(days = 1)
+            save_js_remotely('http://stuhlman.net/gminus/js/ride_data_formatted_'+str(yesterday.month)+'-'+str(yesterday.day)+'.csv',gm_format_csv.format_csv_for_predictions(1))
+            #create a new dataframe for rides
+            df = pd.DataFrame(columns = ['id','name','current_wait','wait_ratio','lat','lon','park','single_rider','lightning_lane','individual_lightning_lane','average_wait','wait_yesterday','wait_in_30'])
+            df = appendRides(df,dis_waits_json)
+            df = appendRides(df,dca_waits_json)
+            df = addLatLon(df)
+            df_dw = pd.DataFrame(columns = ['id','name','current_wait','wait_ratio','lat','lon','park','single_rider','lightning_lane','individual_lightning_lane','average_wait','wait_yesterday','wait_in_30'])
+            df_dw = appendRides(df_dw,epcot_waits_json)
+            df_dw = appendRides(df_dw,mk_waits_json)
+            df_dw = appendRides(df_dw,dhs_waits_json)
+            df_dw = appendRides(df_dw,ak_waits_json)
+            df_dw = addLatLon(df_dw)
+        else:
+            df = pd.read_csv('http://stuhlman.net/gminus/js/ride_data.csv', encoding='utf-8')
+            df_dw = pd.read_csv('http://stuhlman.net/gminus/js/ride_data_dw.csv', encoding='utf-8')
+
+        if (now.minute) < 10:
+            minute_now = "0" + str(now.minute)
+        else:
+            minute_now = str(now.minute) #make sure the minute has two digits so we can sort by minute
+
+        next_col_name = "z" + str(now.year) + "-" + str(now.month) + "-" + str(now.day) + "-" + str(now.hour) + "-" + minute_now #create a new column with the current time
+        df[next_col_name] = ''
+        df_dw[next_col_name] = ''
+        #create a new column with the date/time
+
+        #amend the latest wait times to the newest column
+        df = addWaitTimes(df,dis_waits_json,next_col_name)
+        df = addWaitTimes(df,dca_waits_json,next_col_name)
+        df = updateWaitRatio(df)
+
+        df_dw = addWaitTimes(df_dw,epcot_waits_json,next_col_name)
+        df_dw = addWaitTimes(df_dw,mk_waits_json,next_col_name)
+        df_dw = addWaitTimes(df_dw,dhs_waits_json,next_col_name)
+        df_dw = addWaitTimes(df_dw,ak_waits_json,next_col_name)
+        df_dw = updateWaitRatio(df_dw)
+
+        #convert the dataframe to json
+        waitfile = (df.to_csv(index=False, line_terminator='\n',encoding='utf-8'))
+        waitfile_dw = (df_dw.to_csv(index=False, line_terminator='\n',encoding='utf-8'))
+
+        js_waitfile = clean_waits(df)
+        js_waitfile_dw = clean_waits(df_dw)
+        #create a javascript text with wait times
+
+        #create a json w/ date
+        datefile = "updated = '" + str(now.hour) + ":" + minute_now + ", " + str(now.month) + "/" + str(now.day)+"'"
+
+        #generate a text file for last day updated
+        if (now.month) < 10:
+            month_now = "0" + str(now.month)
+        else:
+            month_now = str(now.month)
+        if (now.day) < 10:
+            day_now = "0" + str(now.day)
+        else:
+            day_now = str(now.day)
+        date_txt = month_now + "/" + day_now
+        date_append_js = str(now.hour) + "-" + minute_now + "-" + str(now.month) + "-" + str(now.day)+"-"
+
+        #open gminus_html_template and add ?date_txt to js/update_date.js and js/ride_data.js
+        gminus_html = requests.get("http://stuhlman.net/gminus/gminus_template.html").text #GET THIS FILE FROM TEMPLATE
+        gminus_dw_html = requests.get("http://stuhlman.net/gminus/gminus_dw_template.html").text #GET THIS FILE
+        gminus_html = rename_js_with_question_mark(gminus_html,date_append_js)
+        gminus_dw_html = rename_js_with_question_mark(gminus_dw_html,date_append_js)
+        save_html_remotely("gminus.html",gminus_html)
+        save_html_remotely("gminus_dw.html",gminus_dw_html) 
+
+        save_js_remotely("ride_data.csv",waitfile) 
+        save_js_remotely("ride_data_"+str(now.month)+"-"+str(now.day)+".csv",waitfile)
+        save_js_remotely("ride_data_dw.csv",waitfile_dw) 
+        save_js_remotely("update_date.js",datefile)
+        save_js_remotely("ride_data.js",js_waitfile)
+        save_js_remotely("ride_data_dw.js",js_waitfile_dw)
+        save_js_remotely("update_date.txt",date_txt)
+        print("Files saved, sleeping")
+        if (do_exactly_twice==0):
+            sleep(297) #we do this every 5 minutes, but it's scheduled for every 10 minutes, so we do it twice instead
+
+main()
