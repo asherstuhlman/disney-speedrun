60cd02b4371cd9c5be29436ea7eb9230ebefee92
asherstuhlman <asherstuhlman@gmail.com>
2022-07-29 17:33:19 -0500

    Update gminus_v3.py

--- gminus_v3.py
+++ gminus_v3.py
@@ -40,7 +40,6 @@ def updateWaitRatio(df):
     #2. Predicted wait / current wait (Is the predicted wait higher than the actual wait right now? Good)
     #3. Predicted wait in 30 minutes / predicted wait (Is the predicted wait now better than the wait in 30 minutes? Good. We use predicted weight as the denominator because this is a ratio, and past days might not be representative of current absolute values for wait)
     last_col = len(df.columns)
-    logFile = []
     #print(df.itertuples())
     if last_col > 20: #if we have enough data points
         for row in df.itertuples():
@@ -84,7 +83,7 @@ def updateWaitRatio(df):
             future_time_label = str(future_time.hour).zfill(2)+':'+str(future_time.minute).zfill(2)
 
             if df.at[row[0],"park"] in ["DL","DCA"]:
-                
+                logFile = []
                 predictedVsCurrentWait = 1
                 predictedFutureWaitTimeUp = 1
                 currentVsAverage = 1
@@ -113,13 +112,14 @@ def updateWaitRatio(df):
 
                 waitRatio = waitTimeGoingDownRatio + predictedVsCurrentWait + predictedFutureWaitTimeUp + currentVsAverage 
                 df.at[row[0],"wait_ratio"] = waitRatio
+                save_js_remotely("logfile.txt",' | '.join(logFile))
             else:
                 df.at[row[0],"wait_ratio"] = waitTimeGoingDownRatio
     else:
         for row in df.itertuples():
             waitRatio = 1
             df.at[row[0],"wait_ratio"] = waitRatio
-    save_js_remotely("logfile.txt",' | '.join(logFile))
+
     return df
 
 #todo: use https://github.com/cubehouse/themeparks for park open/close times so i don't waste cycles
